adff4d33240c6ac84488a1663e4ac9dad4c4c5eb
HaruArima08 <uharubxxcd@gmail.com>
2025-05-16 23:58:36 +0900

    add: extra問題

---
+++ extra/NewYear.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int time;
+    cin >> time;
+    // cout << "12 月 30 日の" << time << "時から次の年になるまでは" << (24 - time) + 24 << "時間です。 " << endl;
+    cout << (24 - time) + 24 << endl;
+}
